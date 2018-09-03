d65f3ec8be80310acaa5664ca585d75ef43f42bb
Saugkim <25344978+saugkim@users.noreply.github.com>
2018-09-03 15:12:40 +0300

    Kimin work
    I might not quite understand what Valtteri means of visualization (let me know if i am far away from the goal, i will try to get there??), i used on click button event to instantiate the ingredient object to the scene(because there are buttons.......with names). And if click the "Puhekupla" then it shows the ordered menu(randomly chosen,,,,,). Question: In Hampurilainen scriptableObject, what is the meaning of layers? the order of layers in unity??

---
+++ SpaceBurgers/Assets/Sandbox/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour {
+
+
+    public List<Hampurilanen> hampurilanens;
+    int orderItemIndex;
+    public GameObject imageHolder;
+
+
+    //public GameObject orderBoard;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        orderItemIndex = UnityEngine.Random.Range(0, 2);
+        Debug.Log(hampurilanens[orderItemIndex].name);
+        //Instantiate(hampurilanens[orderItemIndex], orderBoard.transform.position, transform.rotation);
+        imageHolder.GetComponent<SpriteRenderer>().sprite = hampurilanens[orderItemIndex].Image;
+    }
+}
--- SpaceBurgers/Assets/Scripts/Hampurilanen.cs
+++ SpaceBurgers/Assets/Scripts/Hampurilanen.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="New Burger", menuName= "Burger")]
 public class Hampurilanen : ScriptableObject {
 
-	public string name;
+	public new string name;
 	public Sprite Image;
 	public int layers;
 	public List<int> Ingredients;
