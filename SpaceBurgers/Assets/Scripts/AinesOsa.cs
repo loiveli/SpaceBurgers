2b41882dd64e315690c00aaf461ec4a0583a1fd0
Saugkim <25344978+saugkim@users.noreply.github.com>
2018-09-14 16:33:14 +0300

    Menu Scene
    is it what we want???? ...... help !!!

--- SpaceBurgers/Assets/Scripts/AinesOsa.cs
+++ SpaceBurgers/Assets/Scripts/AinesOsa.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
 [CreateAssetMenu(fileName ="Uusi aines", menuName= "Ingredient")]
 public class AinesOsa : ScriptableObject {
 
---
+++ SpaceBurgers/Assets/Scripts/MenuConstrol.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuConstrol : MonoBehaviour
+{
+
+    public GameObject panel;
+
+    public void StartGame()
+    {
+
+        SceneManager.LoadScene(1);
+    }
+
+    public void ViewOption()
+    {
+        panel.SetActive(true);
+
+    }
+
+    public void ClosePanel()
+    {
+        panel.SetActive(false);
+    }
+}
