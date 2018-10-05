19dcc721b4a8d5806cd8271bdb5f0cb8800c1703
Taavi <42571038+txaxaxvxi@users.noreply.github.com>
2018-10-05 12:41:00 +0300

    Lisätty energiamittari

---
+++ SpaceBurgers/Assets/Scripts/EnergyMeter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyMeter : MonoBehaviour
+{
+    Image EnergyBar0;
+    float maxEnergy = 10000f;
+    public static float Energy;
+
+     void Start()
+    {
+        EnergyBar0 = GetComponent<Image>();
+        Energy = maxEnergy;
+    }
+    void FixedUpdate()
+    {
+        if (Energy > 0)
+        {
+            Energy-=10;
+        }
+        
+        EnergyBar0.fillAmount = Energy / maxEnergy;
+    }
+}
