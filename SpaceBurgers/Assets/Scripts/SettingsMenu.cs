e79a4c3b80dcd72474890bd633e9b55e87af758a
Unknown <valtteri.nietula@gmail.com>
2018-10-09 19:59:12 +0300

    Lisätty alkeellinen Options menu

---
+++ SpaceBurgers/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void GraphicsChange (int indexGFX) {
+		QualitySettings.SetQualityLevel(indexGFX);
+	}
+	
+	// Update is called once per frame
+	public void SoundChange (bool sound) {
+		if(sound){
+
+		}
+	}
+}
